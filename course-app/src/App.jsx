e669a49f0072fd9d6953194e7b82cea5a19ab9ce
dhruba-star <indiracgps@gmail.com>
2025-07-18 22:30:44 +0600

    Update App.jsx

--- course-app/src/App.jsx
+++ course-app/src/App.jsx
@@ -1,9 +1,15 @@
-// src/App.jsx
-import React from "react";
-import CourseListing from "./pages/CourseListing";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Courses from "./pages/Courses.jsx";
+import CourseDetails from "./pages/CourseDetails.jsx";
 
-const App = () => {
-  return <CourseListing />;
-};
-
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Courses />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+      </Routes>
+    </Router>
+  );
+}
